da145226bee34b8d3dfb42f6df26536e0810b18d
Benjamin Boudra <benboudra@outlook.com>
2013-11-03 12:46:11 -0700

    V 1.1.1

--- GitShareTest/src/shareCalculator/ShareCalculator.java
+++ GitShareTest/src/shareCalculator/ShareCalculator.java
@@ -10,10 +10,10 @@ public class ShareCalculator {
 	public void setter() {
 		Scanner sc = new Scanner(System.in);
 		System.out
-		.println("please enter the first number that you wish to multiply");
+		.println("please enter the first number that you wish to operate on");
 		num1 = sc.nextInt();
 		System.out
-		.println("please enter the second number that you wish to multiply");
+		.println("please enter the second number that you wish to operate on");
 		num2 = sc.nextInt();
 		sc.close();
 	}
@@ -26,10 +26,10 @@ public class ShareCalculator {
 		return result;
 	}
 
-	public int divide() {
-		int j = num1;
-		int i = num2;
-		int result = i * j;
+	public double divide() {
+		double j = num1;
+		double i = num2;
+		double result = j/i; // this divides  num1 by num2, choice of variable names can bee a little misleading
 		return result;
 	}
 
@@ -43,7 +43,7 @@ public class ShareCalculator {
 		ShareCalculator c1 = new ShareCalculator();
 		c1.setter();
 		int result = c1.simple();
-		int result2 = c1.divide();
+		double result2 = c1.divide();
 		System.out
 		.println("this is the result of the multiplication operation being printed from the main "
 				+ result);
