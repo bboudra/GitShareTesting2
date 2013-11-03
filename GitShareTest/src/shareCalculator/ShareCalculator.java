0906f98240dd2aae7a30818cadc4c0ab459b6288
Benjamin Boudra <benboudra@outlook.com>
2013-11-03 12:22:16 -0700

    V 1.1
    Added Division Functionality

--- GitShareTest/src/shareCalculator/ShareCalculator.java
+++ GitShareTest/src/shareCalculator/ShareCalculator.java
@@ -6,13 +6,14 @@ public class ShareCalculator {
 
 	private int num1;
 	private int num2;
-	
-	public void setter()
-	{
+
+	public void setter() {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("please enter the first number that you wish to multiply");
+		System.out
+		.println("please enter the first number that you wish to multiply");
 		num1 = sc.nextInt();
-		System.out.println("please enter the second number that you wish to multiply");
+		System.out
+		.println("please enter the second number that you wish to multiply");
 		num2 = sc.nextInt();
 		sc.close();
 	}
@@ -25,18 +26,30 @@ public class ShareCalculator {
 		return result;
 	}
 
+	public int divide() {
+		int j = num1;
+		int i = num2;
+		int result = i * j;
+		return result;
+	}
+
 	public static void main(String[] args) { // notice the very important
-												// Difference here between the
-												// original commit and the one
-												// that i am currently using you
-												// may notice they aren't even
-												// the same program.
+		// Difference here between the
+		// original commit and the one
+		// that i am currently using you
+		// may notice they aren't even
+		// the same program.
 		System.out.println("This is a basic calculator");
 		ShareCalculator c1 = new ShareCalculator();
 		c1.setter();
 		int result = c1.simple();
-		System.out.println("this is the result being printed from the main "
+		int result2 = c1.divide();
+		System.out
+		.println("this is the result of the multiplication operation being printed from the main "
 				+ result);
+		System.out
+		.println("this is the result of the division operation being printed from the main "
+				+ result2);
 	}
-	
+
 }
